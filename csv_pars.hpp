d98ea30fd097376870e84643d7d4356239d295f7
𡡚 <206096109+mknRuSp@users.noreply.github.com>
2025-11-22 22:23:35 +0900

    Add files via upload

---
+++ csv_pars.hpp
@@ -0,0 +1,25 @@
+//CSV parser my mknRuSq
+#ifndef CSV_PARS_IS_INCLUDED
+    #define CSV_PARS_IS_INCLUDED 1
+    #include <string>
+    #include <vector>
+    #include <initializer_list>
+    namespace csv 
+    {
+        auto parsing( const std::string_view CSV_TXT )
+        {
+            std::string temp{};
+            std::vector<std::string> ParsedCsv{};
+            for( const auto &i : CSV_TXT )
+            {
+                if( i == ',' ) 
+                {
+                    ParsedCsv.push_back(temp);
+                    temp.clear();  
+                }
+                else { temp += i; }
+            }
+            return std::initializer_list{ParsedCsv.get_allocator()};
+        }        
+    }
+#endif
